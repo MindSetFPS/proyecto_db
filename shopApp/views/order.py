fd285637c853f1420b953b5aec26a1293c169674
LiliAbi-ne <contrerass.cristina@gmail.com>
2024-09-16 12:57:22 -0700

    Testing files

--- shopApp/views/order.py
+++ shopApp/views/order.py
@@ -1,19 +1,15 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpRequest
+# Example view (shopApp/views/order.py)
+from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
-from shopApp.models import Order, Customer, OrderDetail
+from shopApp.models import Order
 
 @login_required
-def order(request: HttpRequest, order_id):
-    # Redirect if order does not exist
-    
-    order = Order.objects.get(pk = order_id)
-    customer = Customer.objects.filter(user=order.customer).first()
-    products = OrderDetail.objects.filter(order = order_id).all()
-    
+def order(request, order_id):
+    # Get the order object or return a 404 error if it doesn't exist
+    order = get_object_or_404(Order.objects.select_related('category'), id=order_id)
+    products = order.products.all()  # Assuming Order has a related name 'products'
+
+    # Assuming each product has an image set and we need the first image
     for product in products:
-        product.image = product.product.image_set.first()
-    print(order)
-    print(customer)
-    print(products)
-    return render(request, 'order.html', {'order': order, 'customer': customer, 'products': products})
+        product.image = product.image_set.first()
+    return render(request, 'order.html', {'order': order, 'products': products})
