bdd83056812ec5eebd1711f0ed175ac79daeda45
Daniel Escobar <daanielescobar@gmail.com>
2024-09-16 18:59:41 -0600

    happy little accidents

---
+++ shopApp/views/__init__.py
@@ -0,0 +1,7 @@
+# from views import *
+from .order_history import order_history
+from .all import *
+from .order import order
+from .profile import profile
+from .payment import *
+from .buy import buy_cart, success
